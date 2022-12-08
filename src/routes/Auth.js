bbb087bc8a0e5fa37a36ff640dd686817b868a4a
parkyejin <uiop5809@naver.com>
2022-12-08 20:33:14 +0900

    Login Form

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Auth() {
-  return <div>Auth</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleChange = (e) => {
+    const { type, value } = e.target;
+    if (type === "email") {
+      setEmail(value);
+    } else if (type === "password") {
+      setPassword(value);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={handleChange}
+        />
+        <input type="submit" value="Login" />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
 }
