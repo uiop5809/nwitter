cf5e695ec2a6ba10a4fad1e1d3bcf09ba80be4a3
parkyejin <uiop5809@naver.com>
2022-12-11 19:48:03 +0900

    Database setup

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  const [nweet, setNweet] = useState("");
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+  };
+  const handleOnChange = (e) => {
+    const { value } = e.target;
+    setNweet(value);
+  };
+
+  return (
+    <div>
+      <form>
+        <input
+          type="text"
+          value={nweet}
+          onChange={handleOnChange}
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Nweet" onSubmit={handleOnSubmit} />
+      </form>
+    </div>
+  );
 }
